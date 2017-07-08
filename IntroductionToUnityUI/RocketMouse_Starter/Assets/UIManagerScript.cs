82f2d851bef7671e85c0c8eee6a5e62b78cd8f6d
minhpl <minhplhl@gmail.com>
2017-07-08 21:10:32 +0700

    UILearn

--- IntroductionToUnityUI/RocketMouse_Starter/Assets/UIManagerScript.cs
+++ IntroductionToUnityUI/RocketMouse_Starter/Assets/UIManagerScript.cs
@@ -3,11 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class UIManagerScript : MonoBehaviour {
+public class UIManagerScript : MonoBehaviour
+{
+
+    public Animator startButton;
+    public Animator settingsButton;
+    public Animator dialog;
+
+    public void OpenSettings()
+    {
+        startButton.SetBool("isHidden", true);
+        settingsButton.SetBool("isHidden", true);
+        dialog.SetBool("isHidden", false);
+    }
+
 
     public void StartGame()
     {
         SceneManager.LoadScene("RocketMouse", LoadSceneMode.Single);
+    }
+
+    public void CloseSettings()
+    {
+        startButton.SetBool("isHidden", false);
+        settingsButton.SetBool("isHidden", false);
+        dialog.SetBool("isHidden", true);
     }
 
 }
