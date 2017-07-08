828e197f3f6b6e67a12faa22976b90ef0f7d84b5
minhpl <minhplhl@gmail.com>
2017-07-08 21:10:08 +0700

    Assets

--- Assets/Scripts/HomeController.cs
+++ Assets/Scripts/HomeController.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class HomeController : MonoBehaviour {
 
+    public GameObject go;
 	// Use this for initialization
 	void Start () {
-		
+        go.tag = "id";
+        UIEventListener.Get(go).onClick += LoadOtherScreen2;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void LoadOtherScreen()
+    {
+        //
+    }
+    public void LoadOtherScreen2(GameObject sender)
+    {
+        //
+    }
 }
