334f39bc2dd7e33707d5448a72270b4b10ddd3bc
minhpl <minhplhl@gmail.com>
2017-07-11 22:22:48 +0700

    Assets

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour {
+    public GameObject content;
+	public void Print()
+    {
+        //Image a = content.GetComponent<Image>();
+        //Debug.Log(a.mainTexture.width); 
+    }
+
+}
