7d65ee411e80103e9215a6c9ba5cf667d392ce01
minhpl <minhplhl@gmail.com>
2017-07-19 20:45:23 +0700

    Assets

---
+++ Assets/Scripts/MakePersistentObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakePersistentObject : MonoBehaviour {
+
+    public static MakePersistentObject Instance;
+
+
+    private void Awake()
+    {
+        this.InstantiateController();
+    }
+
+    private void Start()
+    {
+        //Debug.LogFormat("Xin chao dong chi");
+    }
+
+    private void InstantiateController()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);            
+        }
+        else if (this != Instance)
+        {
+            if (!Instance.gameObject.activeSelf) Instance.gameObject.SetActive(true);
+            Destroy(this.gameObject);
+        }
+    }
+
+}
---
+++ Assets/Scripts/TestTextureUVCoordinate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestTextureUVCoordinate : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        Image a = GetComponent<Image>();
+        var name = a.gameObject.name;
+        var uv = a.sprite.uv;
+        Debug.LogFormat("texture name is {0} have length uv is {1}", name, a.sprite.rect.ToString());
+        for (int i = 0; i < uv.Length; i++)
+            Debug.LogFormat(uv[i].ToString());
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
