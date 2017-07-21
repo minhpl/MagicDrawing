b070de57a8602cee0081efd1cb6ddbceedd988b2
minhpl <minhplhl@gmail.com>
2017-07-21 18:47:52 +0700

    using threads job

---
+++ Assets/Scripts/Job.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using System;
+using UnityEngine;
+using OpenCVForUnity;
+using System.Collections;
+using System.Collections.Generic;
+
+using OpenCVForUnityExample;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Job : ThreadedJob
+{
+
+    public Mat image;
+    public AdaptiveThreshold athreshold;
+    public Utilities utilities;
+    public RawImage rimgmodel;
+    public Texture2D texEdges;
+
+    private Color32[] colorsBuffer;
+
+
+    protected override void ThreadFunction(Handler handle)
+    {
+
+        Mat edges = athreshold.adapTiveThreshold(image);
+
+        Mat tempMat = new Mat();
+        Core.bitwise_not(edges, tempMat);
+
+        Mat redMat = new Mat();
+
+        List<Mat> listMat = new List<Mat>();
+        Mat zeroMat = Mat.zeros(tempMat.size(), CvType.CV_8U);
+        listMat.Add(tempMat);
+        listMat.Add(zeroMat);
+        listMat.Add(zeroMat);
+        listMat.Add(tempMat);
+        Core.merge(listMat, redMat);
+
+        utilities.set(edges);
+        colorsBuffer = new Color32[edges.width() * edges.height()];
+
+        handle(redMat);
+        
+        //redMat.Dispose();
+    }
+}
---
+++ Assets/Scripts/ThreadedJob.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using OpenCVForUnity;
+
+public class ThreadedJob
+{
+    private bool m_IsDone = true;
+    private object m_Handle = new object();
+    private System.Threading.Thread m_Thread = null;
+    public bool IsDone
+    {
+        get
+        {
+            bool tmp;
+            lock (m_Handle)
+            {
+                tmp = m_IsDone;
+            }
+            return tmp;
+        }
+        set
+        {
+            lock (m_Handle)
+            {
+                m_IsDone = value;
+            }
+        }
+    }
+
+    public delegate void Handler(Mat data);
+    public virtual void Start(Handler handle)
+    {
+        this.handle = handle;
+        m_Thread = new System.Threading.Thread(Run);
+        m_Thread.Start();
+    }
+    public virtual void Abort()
+    {
+        m_Thread.Abort();
+    }
+    Handler handle;
+    protected virtual void ThreadFunction(Handler handle)
+    {
+    }
+
+    protected virtual void OnFinished()
+    {
+        m_Thread = null;
+    }
+
+    public virtual bool Update()
+    {
+        if (IsDone)
+        {
+            OnFinished();
+            return true;
+        }
+        return false;
+    }
+    public IEnumerator WaitFor()
+    {
+        while (!Update())
+        {
+            yield return null;
+        }
+    }
+    private void Run()
+    {
+        ThreadFunction(handle);
+        IsDone = true;
+    }
+}
