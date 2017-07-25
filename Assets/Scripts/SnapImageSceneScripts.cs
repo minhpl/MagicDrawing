1019259e421cfd1b6b64d2d303be7cfd4d80e541
minhpl <minhplhl@gmail.com>
2017-07-26 02:08:17 +0700

    c

--- Assets/Scripts/SnapImageSceneScripts.cs
+++ Assets/Scripts/SnapImageSceneScripts.cs
@@ -14,52 +14,89 @@ public class SnapImageSceneScripts : MonoBehaviour {
     private bool camAvailable;
     private WebCamTexture webcamTex;
     private WebCamDevice webCamDevice;
-    private bool isFronFacing = false;
-    private int CamWidth = 4800;
-    private int CamHeight = 6400;
+    private bool isFronFacing = true;
+    private int requestWidth = 4800;
+    private int requestHeight = 6400;
     // Use this for initialization
     private WebCamTextureToMatHelper webcamTextureTomat;
     void Start () {
         rawImgCam = goCam.GetComponent<RawImage>();
         webcamTextureTomat = goCam.GetComponent<WebCamTextureToMatHelper>();
-        webcamTextureTomat.Init(null, webcamTextureTomat.requestWidth, webcamTextureTomat.requestHeight, webcamTextureTomat.requestIsFrontFacing);
-        //WebCamDevice[] devices = WebCamTexture.devices;
-        //if (devices.Length == 0)
-        //{
-        //    Utilities.Log("No camera detected");
-        //    camAvailable = false;
-        //    return;
-        //}
+        //webcamTextureTomat.Init(null, webcamTextureTomat.requestWidth, webcamTextureTomat.requestHeight, webcamTextureTomat.requestIsFrontFacing);
+
+        Screen.autorotateToLandscapeLeft = false;
+        Screen.autorotateToLandscapeRight = false;
+        Screen.autorotateToPortrait = false;
+        Screen.autorotateToPortraitUpsideDown = false;
+
+        WebCamDevice[] devices = WebCamTexture.devices;
+        if (devices.Length == 0)
+        {
+            Utilities.Log("No camera detected");
+            camAvailable = false;
+            return;
+        }
+
         //Utilities.Log("how many camera {0}", devices.Length);
 
-        //for (int i = 0; i < devices.Length; i++)
-        //{
-        //    if (devices[i].isFrontFacing == isFronFacing)
-        //    {
-        //        webcamTex = new WebCamTexture(devices[i].name, CamWidth, CamHeight);
-        //        webCamDevice = devices[i];
-        //        break;
-        //    }               
-        //}        
-        //if (webcamTex == null)
-        //{
-        //    Utilities.Log("Unable to find back camera");
-        //    webcamTex = new WebCamTexture(devices[0].name, CamWidth, CamHeight);
-        //    webCamDevice = devices[0];            
-        //}
-        //webcamTex.Play();
-        ////if (webCamDevice.isFrontFacing == true)
-        ////    rawImgCam.rectTransform.localScale = new Vector3(-1, 1, 1);
-        ////if(webCamDevice.isFrontFacing == false)
-        ////    rawImgCam.rectTransform.localScale = new Vector3(-1, 1, 1);
-        //rawImgCam.texture = webcamTex;
-        //camAvailable = true;
-        //var width = webcamTex.width;
-        //var height = webcamTex.height;
-        //var ratio = width / (float)height;
-        //goCam.GetComponent<AspectRatioFitter>().aspectRatio = ratio;
-        
-        MainThreadDispatcher.StartUpdateMicroCoroutine(Worker());
+        for (int i = 0; i < devices.Length; i++)
+        {
+            if (devices[i].isFrontFacing == isFronFacing)
+            {
+                webcamTex = new WebCamTexture(devices[i].name, requestWidth, requestHeight);
+                webCamDevice = devices[i];
+                break;
+            }
+        }
+
+        if (webcamTex == null)
+        {
+            Utilities.Log("Unable to find back camera");
+            webcamTex = new WebCamTexture(devices[0].name, requestWidth, requestHeight);
+            webCamDevice = devices[0];
+        }
+
+        webcamTex.Play();
+        rawImgCam.texture = webcamTex;
+
+
+        var widthCam = webcamTex.width;
+        var heightCam = webcamTex.height;
+
+        var ratioWH = widthCam / (float)heightCam;
+        var ratioHW = heightCam / (float)widthCam;
+        Utilities.Log("Webcam width is {0}, webcam height is {1}", widthCam, heightCam);
+
+        var widthDisplay = rawImgCam.rectTransform.rect.width;
+        var heightDisplay = rawImgCam.rectTransform.rect.height;
+        var ratioDisplay = widthDisplay / (float)heightDisplay;
+
+        int orient = webcamTex.videoRotationAngle;
+        Utilities.Log("orient here is {0}", orient);
+        if(orient==90 || orient==270)
+        {
+            if (ratioHW < ratioDisplay)
+            {
+                var newHeight = widthDisplay;
+                var newWidth = newHeight * ratioWH;
+
+                var heightDelta = newHeight - heightDisplay;
+                var widthDelta = newWidth - widthDisplay;
+                rawImgCam.rectTransform.sizeDelta = new Vector2(widthDelta, heightDelta);
+            }
+            else
+            {
+                var newWidth = heightDisplay;
+                var newHeight = newWidth * ratioHW;
+
+                var heightDelta = newHeight - heightDisplay;
+                var widthDelta = newWidth - widthDisplay;
+                rawImgCam.rectTransform.sizeDelta = new Vector2(widthDelta, heightDelta);
+            }
+        }        
+        rawImgCam.rectTransform.localEulerAngles = new Vector3(0, 0, -orient);
+        camAvailable = true;
+        //MainThreadDispatcher.StartUpdateMicroCoroutine(Worker());
     }
 
     IEnumerator Worker()
@@ -67,24 +104,9 @@ public class SnapImageSceneScripts : MonoBehaviour {
         while (true)
         {
             yield return null;
-            //if(camAvailable)
+            if (camAvailable)
             {
-
-                webcamTex = webcamTextureTomat.GetWebCamTexture();
-                var width = webcamTex.width;
-                var height = webcamTex.height;
-                var ratio = width / (float)height;
-                goCam.GetComponent<AspectRatioFitter>().aspectRatio = ratio;
-                Utilities.Log("width is {0}, height is {1}", width, height);
-                rawImgCam.texture = webcamTex;
-
-                //long time = DateTime.Now.Millisecond;
-                float scaleY = webcamTex.videoVerticallyMirrored ? -1f : 1f;
-                int orient = webcamTex.videoRotationAngle;
-                Utilities.Log("orient is {0}, videoVerticallyMirrored is {1}", orient, webcamTex.videoVerticallyMirrored);
-                rawImgCam.rectTransform.localEulerAngles = new Vector3(0, 0, -orient);
-                //rawImgCam.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
-                //Utilities.Log("Excution time is {0}", DateTime.Now.Millisecond - time);
+                               
             }
         }
     }
@@ -100,6 +122,8 @@ public class SnapImageSceneScripts : MonoBehaviour {
         webcamTex.Stop();
         webcamTex = null;
         WebCamDevice[] devices = WebCamTexture.devices;
+        rawImgCam.rectTransform.sizeDelta = new Vector2(0, 0);
+        rawImgCam.rectTransform.localEulerAngles = new Vector3(0, 0, 0);
 
         isFronFacing = !isFronFacing;
         for (int i = 0; i < devices.Length; i++)
@@ -107,27 +131,63 @@ public class SnapImageSceneScripts : MonoBehaviour {
             if (devices[i].isFrontFacing == isFronFacing)
             {
                 webCamDevice = devices[i];
-                webcamTex = new WebCamTexture(devices[i].name, 480, 680);
-                
+                webcamTex = new WebCamTexture(devices[i].name, requestWidth, requestHeight);
+
                 break;
             }
         }
 
         if (webcamTex == null && devices.Length > 0)
         {
-            webcamTex = new WebCamTexture(devices[0].name, 480, 680);
+            webcamTex = new WebCamTexture(devices[0].name, requestWidth, requestHeight);
             webCamDevice = devices[0];
         }
-            
 
         webcamTex.Play();
+
+        var widthCam = webcamTex.width;
+        var heightCam = webcamTex.height;
+
+        var ratioWH = widthCam / (float)heightCam;
+        var ratioHW = heightCam / (float)widthCam;
+        Utilities.Log("Webcam width is {0}, webcam height is {1}", widthCam, heightCam);
+
+        var widthDisplay = rawImgCam.rectTransform.rect.width;
+        var heightDisplay = rawImgCam.rectTransform.rect.height;
+        var ratioDisplay = widthDisplay / (float)heightDisplay;
+
+        int orient = webcamTex.videoRotationAngle;
+        Utilities.Log("orient is {0}", orient);
+
+
+        if (isFronFacing == false)
+        {
+            if (orient == 90 || orient == 270)
+            {
+                if (ratioHW < ratioDisplay)
+                {
+                    var newHeight = widthDisplay;
+                    var newWidth = newHeight * ratioWH;
+
+                    var heightDelta = newHeight - heightDisplay;
+                    var widthDelta = newWidth - widthDisplay;
+                    rawImgCam.rectTransform.sizeDelta = new Vector2(widthDelta, heightDelta);
+                }
+                else
+                {
+                    var newWidth = heightDisplay;
+                    var newHeight = newWidth * ratioHW;
+
+                    var heightDelta = newHeight - heightDisplay;
+                    var widthDelta = newWidth - widthDisplay;
+                    rawImgCam.rectTransform.sizeDelta = new Vector2(widthDelta, heightDelta);
+                }
+                
+            }
+        }
+
+
+        rawImgCam.rectTransform.localEulerAngles = new Vector3(0, 0, -orient);
         rawImgCam.texture = webcamTex;
-        if (webCamDevice.isFrontFacing==true)
-            rawImgCam.rectTransform.localScale = new Vector3(-1, 1, 1);
-        //else
-        //    rawImgCam.rectTransform.localScale = new Vector3(1, -1, 1);
-        rawImgCam.texture = webcamTex;
-        //optimizationWebCamTextureToMatHelper.Init(null, optimizationWebCamTextureToMatHelper.requestWidth,
-        //    optimizationWebCamTextureToMatHelper.requestHeight, !optimizationWebCamTextureToMatHelper.requestIsFrontFacing);
     }       
 }
