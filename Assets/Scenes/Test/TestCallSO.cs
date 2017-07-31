1a8f4acbbd66487125c3858f25b2854b11eb2eb1
minhpl <minhplhl@gmail.com>
2017-07-31 20:04:18 +0700

    test native android plugin

---
+++ Assets/Scenes/Test/TestCallSO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class TestCallSO : MonoBehaviour
+{
+
+    void Start()
+    {
+        try
+        {
+            int width = 1;
+            int height = 2;
+
+            var result = ndktestop.SayHello(ref width, ref height);
+
+            Utilities.Log("Width is {0}, height is {1}, and the result is {0}", width, height, result);
+        }
+        catch (Exception e)
+        {
+            Utilities.Log("Error is {0}", e.ToString());
+            Utilities.Log("Trace is {0}", e.StackTrace.ToString());
+        }
+    }
+
+
+    internal static class ndktestop
+    {
+        [DllImport("ndktest")]
+        internal static extern int SayHello(ref int outCameraWidth, ref int outCameraHeight);
+    }
+}
