80d2703740477153563f37a58c9e2b4317b02d83
minhpl <minhplhl@gmail.com>
2017-10-18 19:10:40 +0700

    commit

--- GEOS/Source.cpp
+++ GEOS/Source.cpp
@@ -1,13 +1,22 @@
-// my first program in C++
 #include <iostream>
 #include <geos\geom\MultiPolygon.h>
+#include <geos\geom\GeometryFactory.h>
+#include "geos.h"
 
 using namespace geos::geom;
 
 int main()
 {
-	
+	int size = 10;
+	auto geometryFactory = GeometryFactory::create();
+	auto coordinateSequenceFactory = geometryFactory->getCoordinateSequenceFactory();
+	auto coordinateSequence = coordinateSequenceFactory->create();
 
-
+	coordinateSequence->add(Coordinate(0, 0));
+	coordinateSequence->add(Coordinate(10, 0));
+	coordinateSequence->add(Coordinate(0, 10));
+	coordinateSequence->add(Coordinate(0, 0));	
 	
+	auto linearRing = geometryFactory->createLinearRing(coordinateSequence);
+
 }
