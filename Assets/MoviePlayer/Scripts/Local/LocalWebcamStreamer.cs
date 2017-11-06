83d6d37c1efcc7b5dbcb721ef06cf475999b0f2d
minhpl <minhplhl@gmail.com>
2017-11-06 14:59:17 +0700

    a

--- Assets/MoviePlayer/Scripts/Local/LocalWebcamStreamer.cs
+++
@@ -1,2 +0,0 @@
-// LocalWebcamStreamer is no longer supported, because it was almost never used.
-// If you were using this feature, then revert back to version 0.9
--- Assets/Scripts/DataBind.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class DataBind : MonoBehaviour {
-    public TemplateDrawing drawingTemplateModel = null; 
-    public string imagePath = null;
-    public string videoPath = null;
-}
