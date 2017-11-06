1ada137ad9220f3973ced4765f44c587063867d4
minhpl <minhplhl@gmail.com>
2017-11-06 14:19:27 +0700

    cm

--- OpenCV/Source.cpp
+++ OpenCV/Source.cpp
@@ -1,32 +1,45 @@
-#include "opencv2/opencv.hpp"
-#include "opencv2/videoio/videoio.hpp"
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
-
-using namespace std;
 using namespace cv;
 
-int main() {
+int main()
+{
+	// Create black empty images
+	Mat image = Mat::zeros(400, 400, CV_8UC3);
 
-	VideoCapture vcap(0);
-	if (!vcap.isOpened()) {
-		cout << "Error opening video stream or file" << endl;
-		return -1;
-	}
+	int w = 400;
+	// Draw a circle 
+	/** Create some points */
+	Point rook_points[1][20];
+	rook_points[0][0] = Point(w / 4.0, 7 * w / 8.0);
+	rook_points[0][1] = Point(3 * w / 4.0, 7 * w / 8.0);
+	rook_points[0][2] = Point(3 * w / 4.0, 13 * w / 16.0);
+	rook_points[0][3] = Point(11 * w / 16.0, 13 * w / 16.0);
+	rook_points[0][4] = Point(19 * w / 32.0, 3 * w / 8.0);
+	rook_points[0][5] = Point(3 * w / 4.0, 3 * w / 8.0);
+	rook_points[0][6] = Point(3 * w / 4.0, w / 8.0);
+	rook_points[0][7] = Point(26 * w / 40.0, w / 8.0);
+	rook_points[0][8] = Point(26 * w / 40.0, w / 4.0);
+	rook_points[0][9] = Point(22 * w / 40.0, w / 4.0);
+	rook_points[0][10] = Point(22 * w / 40.0, w / 8.0);
+	rook_points[0][11] = Point(18 * w / 40.0, w / 8.0);
+	rook_points[0][12] = Point(18 * w / 40.0, w / 4.0);
+	rook_points[0][13] = Point(14 * w / 40.0, w / 4.0);
+	rook_points[0][14] = Point(14 * w / 40.0, w / 8.0);
+	rook_points[0][15] = Point(w / 4.0, w / 8.0);
+	rook_points[0][16] = Point(w / 4.0, 3 * w / 8.0);
+	rook_points[0][17] = Point(13 * w / 32.0, 3 * w / 8.0);
+	rook_points[0][18] = Point(5 * w / 16.0, 13 * w / 16.0);
+	rook_points[0][19] = Point(w / 4.0, 13 * w / 16.0);
 
-	int frame_width = vcap.get(CV_CAP_PROP_FRAME_WIDTH);
-	int frame_height = vcap.get(CV_CAP_PROP_FRAME_HEIGHT);	
-	VideoWriter video("out.mp4", -1, 10, Size(frame_width, frame_height), true);
-	
-	auto isopened = video.isOpened();
-	std::cout << "isopened is " << isopened << std::endl;
-	for (;;) {
+	const Point* ppt[1] = { rook_points[0] };
+	int npt[] = { 20 };
 
-		Mat frame;
-		vcap >> frame;
-		video.write(frame);
-		imshow("Frame", frame);
-		char c = (char)waitKey(33);
-		if (c == 27) break;
-	}
-	return 0;
+	fillPoly(image, ppt, npt, 1, Scalar(255, 255, 255), 8);
+	auto b = imwrite("/data/local/tmp/output.png", image);
+	std::cout << "Xin chao tat ca the gioi nay :" << b << std::endl;
+	//waitKey(0);
+	return(0);
 }
