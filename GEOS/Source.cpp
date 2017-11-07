ce494fce316cbfc60cfbfcfbf0f411d996e2980e
minhpl <minhplhl@gmail.com>
2017-11-07 07:37:21 +0700

    cm

--- GEOS/Source.cpp
+++ GEOS/Source.cpp
@@ -4,13 +4,11 @@
 #include <geos.h>
 #include <math.h>
 #include <geos\algorithm\CGAlgorithms.h>
-
 #include <list>
 #include <boost/geometry.hpp>
 #include <boost/geometry/geometries/point_xy.hpp>
 #include <boost/geometry/geometries/polygon.hpp>
 #include <boost/foreach.hpp>
-
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -24,73 +22,88 @@ namespace trans = boost::geometry::strategy::transform;
 
 typedef boost::geometry::model::d2::point_xy<double> point, vector2d;
 typedef boost::geometry::model::polygon<boost::geometry::model::d2::point_xy<double>> polygon;
-typedef boost::geometry::model::multi_polygon<polygon> mpolygon;
+typedef boost::geometry::model::multi_polygon<polygon, std::vector> mpolygon;
 const long double pi = boost::math::constants::pi<long double>();
 
-inline double angle(const vector2d& v1,const vector2d& v2)
+const int MAXSIZE = 600;
+cv::Point polygonarray[1][35];
+const double MIN_AREA_THRESHOLD = 0.1f;
+
+inline double angle(const vector2d& v1, const vector2d& v2)
 {
 	auto dot = dot_product(v1, v2);
 	auto det = v1.x()*v2.y() - v1.y()*v2.x();
 	return atan2(det, dot);
 }
 
-//template<int D>
-//inline double round(const double& value)
-//{
-//	int t = pow(10, D);
-//	return round(value*t) / t;	
-//}
+template<int D>
+inline double round(const double& value)
+{
+	int t = pow(10, D);
+	return round(value*t) / t;
+}
 
 template<int D>
 inline void round(point& p)
 {
 	using boost::geometry::get;
 	using boost::geometry::set;
-	int t = pow(10,D);		
+	int t = pow(10, D);
 	set<0>(p, round(p.x()*t) / t);
 	set<1>(p, round(p.y()*t) / t);
 }
 
-const int MAXSIZE = 600;
 
-void imgwrite(cv::Mat& mat, const polygon& poly)
+inline void polywrite(cv::Mat mat, const polygon& poly, Scalar& color = Scalar(0, 255, 255))
 {
 	auto outer = poly.outer();
 	auto inners = poly.inners();
-	cv::Point pt_outer[1][30];
-	const cv::Point *pts[1] = { pt_outer[0] };
-	
-	auto num = num_points(outer);
-	if (num > 30) throw "cannot print the poly, because numpoints exceed limit ";
-	
+	cv::Point polygonarray[1][30];
+	const cv::Point *pts[1] = { polygonarray[0] };
+
+	auto num = num_points(outer) - 1;
+	std::cout << "num = " << num << std::endl;
+	if (num > 35) throw "cannot print the poly, because num points exceed limit ";
+
 	int npts[1] = { num };
 
-	for (int i =0;i<num-1;i++)
+	for (int i = 0; i < num; i++)
 	{
 		auto p = outer.at(i);
-		pt_outer[0][i] = cv::Point(p.x()*10, p.y()*10);
-	}	 
+		polygonarray[0][i] = cv::Point(p.x() * 10, p.y() * 10);
+		std::cout << "point "<<i<<" :"<<p.x() << " " << p.y() << std::endl;
+	}
 	
-	cv::fillPoly(mat, pts, npts, 1, Scalar(255, 255, 255), 1);
-	//auto b = imwrite("./a.png", mat);
-	//std::cout << "is writed ? " << b << std::endl;
+	cv::fillPoly(mat, pts, npts, 1, color, 8);
+
+	BOOST_FOREACH(auto ring, inners)
+	{
+		std::cout << "are you here???";
+		num = num_points(ring);
+		if (num > 30) throw "cannot print the poly, because num points exceed limit ";
+		npts[0] = num;
+		for (int i = 0; i < num; i++)
+		{
+			auto p = ring.at(i);
+			polygonarray[0][i] = cv::Point(p.x() * 10, p.y() * 10);
+		}
+		cv::fillPoly(mat, pts, npts, 1, Scalar(0, 0, 0), 1);
+	}
+
+	auto b = imwrite("./a.png", mat);
+	std::cout << "is writed ? " << b << std::endl;
 }
 
-inline void imgwrite(const std::string& filename, const polygon& poly)
+inline void polywrite(cv::Mat& mat, const mpolygon& mpoly)
 {
-	
-}
-
-inline void imgwrite(const std::string& filename, const mpolygon& mpoly)
-{
-
+	BOOST_FOREACH(auto poly, mpoly)
+	{
+		polywrite(mat, poly);
+	}
 }
 
 int main()
 {
-	//auto geometryFactory = GeometryFactory::create();
-	//auto coordinateSequenceFactory = geometryFactory->getCoordinateSequenceFactory();
-	//
 	double sizeLargeTriangular = 20;
 	double sizeMediumTriangular = 10 * sqrt(2);
 	double sizeSmallTriangular = 10;
@@ -99,82 +112,6 @@ int main()
 	double sizeLongParallelogram = 10 * sqrt(2);
 	double distanceParallelogram = 5 * sqrt(2);
 	double sizeSquareSilhouette = 20 * sqrt(2);
-	//std::cout << sizeMediumTriangular << std::endl;
-	//std::cout << sizeSquareSilhouette << std::endl;
-	//MultiPolygon* silhouette;
-
-	//LinearRing* LargeTriangular1;
-	//LinearRing* LargeTriangular2;
-	//LinearRing* MediumTriangular;
-	//LinearRing* SmallTriangular1;
-	//LinearRing* SmallTriangular2;
-	//LinearRing* Parallelogram1;
-	//LinearRing* Parallelogram2;  //reflect
-	//LinearRing* Square;
-	//
-	//auto coordSeqLargeTriang1 = coordinateSequenceFactory->create();
-	//coordSeqLargeTriang1->add(Coordinate(0, 0));
-	//coordSeqLargeTriang1->add(Coordinate(0, sizeLargeTriangular));
-	//coordSeqLargeTriang1->add(Coordinate(sizeLargeTriangular, 0));
-	//coordSeqLargeTriang1->add(Coordinate(0, 0));
-	//LargeTriangular1 = geometryFactory->createLinearRing(coordSeqLargeTriang1);
-	//LargeTriangular2 = dynamic_cast<LinearRing *>(LargeTr iangular1->clone());
-	//auto coordSeqMediumTriang = coordinateSequenceFactory->create();
-	//coordSeqMediumTriang->add(Coordinate(0, 0));
-	//coordSeqMediumTriang->add(Coordinate(0, sizeMediumTriangular));
-	//coordSeqMediumTriang->add(Coordinate(sizeMediumTriangular,0));
-	//coordSeqMediumTriang->add(Coordinate(0, 0));
-	//MediumTriangular = geometryFactory->createLinearRing(coordSeqMediumTriang);
-	//auto coordSeqSmallTriang = coordinateSequenceFactory->create();
-	//coordSeqSmallTriang->add(Coordinate(0, 0));
-	//coordSeqSmallTriang->add(Coordinate(0, sizeSmallTriangular));
-	//coordSeqSmallTriang->add(Coordinate(sizeSmallTriangular,0));
-	//coordSeqSmallTriang->add(Coordinate(0, 0));
-	//SmallTriangular1 = geometryFactory->createLinearRing(coordSeqSmallTriang);
-	//SmallTriangular2 = dynamic_cast<LinearRing*>(SmallTriangular1->clone());
-	//auto coordSeqParallelogram1 = coordinateSequenceFactory->create();
-	//coordSeqParallelogram1->add(Coordinate(0, distanceParallelogram));
-	//coordSeqParallelogram1->add(Coordinate(0, distanceParallelogram+sizeLongParallelogram));
-	//coordSeqParallelogram1->add(Coordinate(distanceParallelogram,sizeLongParallelogram));
-	//coordSeqParallelogram1->add(Coordinate(distanceParallelogram, 0));
-	//coordSeqParallelogram1->add(Coordinate(0, distanceParallelogram));
-	//Parallelogram1 = geometryFactory->createLinearRing(coordSeqParallelogram1);
-	//auto coordSeqParallelogram2 = coordinateSequenceFactory->create();
-	//coordSeqParallelogram2->add(Coordinate(0, 0));
-	//coordSeqParallelogram2->add(Coordinate(0, sizeLongParallelogram));
-	//coordSeqParallelogram2->add(Coordinate(distanceParallelogram,distanceParallelogram+ sizeLongParallelogram));
-	//coordSeqParallelogram2->add(Coordinate(distanceParallelogram, distanceParallelogram));
-	//coordSeqParallelogram2->add(Coordinate(0, 0));
-	//Parallelogram2 = geometryFactory->createLinearRing(coordSeqParallelogram2);
-	//auto coordSeqSquare = coordinateSequenceFactory->create();
-	//coordSeqSquare->add(Coordinate(0, 0));
-	//coordSeqSquare->add(Coordinate(0, 10));
-	//coordSeqSquare->add(Coordinate(10, 10));
-	//coordSeqSquare->add(Coordinate(10, 0));
-	//coordSeqSquare->add(Coordinate(0, 0));
-	//Square = geometryFactory->createLinearRing(coordSeqSquare);
-
-	//auto coordSeqSquareSilhouette = coordinateSequenceFactory->create();
-	//coordSeqSquareSilhouette->add(Coordinate(0, 0));	
-	//coordSeqSquareSilhouette->add(Coordinate(0, sizeSquareSilhouette));
-	//coordSeqSquareSilhouette->add(Coordinate(sizeSquareSilhouette, sizeSquareSilhouette));
-	//coordSeqSquareSilhouette->add(Coordinate(sizeSquareSilhouette, 0));
-	//coordSeqSquareSilhouette->add(Coordinate(0, 0));
-	//std::vector<Geometry*> *geos = new vector<Geometry*>();
-	//auto geometry = (Geometry*)geometryFactory->createLinearRing(coordSeqSquareSilhouette);
-	//geos->push_back(geometry);
-	//silhouette = geometryFactory->createMultiPolygon(geos);
-	//
-
-	//auto isCCW = CGAlgorithms::isCCW(LargeTriangular1->getCoordinatesRO());
-	//std::cout << (LargeTriangular1 == nullptr) << std::endl;
-	//std::cout << (LargeTriangular2 == nullptr) << std::endl;
-	//std::cout << (LargeTriangular2 == nullptr) << std::endl;
-	//std::cout << (coordSeqLargeTriang1 == coordSeqMediumTriang) << std::endl;
-	//std::cout << (Parallelogram1->isClosed()) << std::endl;
-	//std::cout << isCCW << std::endl;
-	//std::cout << LargeTriangular1->getCoordinatesRO()->getDimension() << std::endl;
-	//std::cout << jtsport() << std::endl;	
 
 	polygon largeTriangular1{ { { 0,0 },{ sizeLargeTriangular, 0 } ,{ 0, sizeLargeTriangular },{ 0,0 } } };
 	polygon largeTriangular2{ largeTriangular1 };
@@ -204,7 +141,7 @@ int main()
 	tagramPieces.push_back(largeTriangular1);
 	tagramPieces.push_back(largeTriangular2);
 	tagramPieces.push_back(parallelogram1);
-	//tagramPieces.push_back(parallelogram2);
+	tagramPieces.push_back(parallelogram2);
 	tagramPieces.push_back(mediumTriangular);
 	tagramPieces.push_back(square);
 	tagramPieces.push_back(smallTriangular1);
@@ -212,9 +149,17 @@ int main()
 
 	auto outerRing = silhouettePolygon.outer();
 
+	//std::cout << dsv(outerRing) << std::endl;
+	//std::cout << dsv(largeTriangular1) << std::endl;
+	//std::vector<polygon> dif;
+	//difference(outerRing, largeTriangular1, dif);
+	//std::cout << area(dif[0]) << std::endl;
+
+
 	Mat mat = Mat::zeros(MAXSIZE, MAXSIZE, CV_8UC3);
 	try {
-		imgwrite(mat, silhouettePolygon);
+		//polywrite(mat, largeTriangular1);
+		//polywrite(mat, parallelogram2);
 	}
 	catch (char* e)
 	{
@@ -222,110 +167,89 @@ int main()
 	}
 
 	if (num_points(outerRing) > 1) {
-		for (auto it = outerRing.begin(); it != outerRing.end()-1 ; it++)
+		for (auto it = outerRing.begin(); it != outerRing.end() - 1; it++)
 		{
 			auto& p1 = *it;
 			auto p2 = *(it + 1);
 			auto& vectorEdge = p2;
-			subtract_point(vectorEdge,p1);
-			//std::cout << vectorEdge.x() << "  " <<vectorEdge.y()<<std::endl;
-			BOOST_FOREACH(auto& a, tagramPieces)
-			{				
-				auto outerTagramPieces = a.outer();
+			subtract_point(vectorEdge, p1);
+			//std::cout << vectorEdge.x() << "  " << vectorEdge.y() << std::endl;
+			BOOST_FOREACH(auto& tagramP, tagramPieces)
+			{
+				auto outerTagramPieces = tagramP.outer();
 				for (auto it2 = outerTagramPieces.begin(); it2 != outerTagramPieces.end() - 1; it2++)
 				{
 					auto pPiece1 = *it2;
 					auto pPiece2 = *(it2 + 1);
 					auto& vectorEdgePieces = pPiece2;
-					subtract_point(vectorEdgePieces , pPiece1);
-					auto ang = angle(vectorEdge,vectorEdgePieces);
+					subtract_point(vectorEdgePieces, pPiece1);
+					auto ang = angle(vectorEdge, vectorEdgePieces);
 					auto degree = ang * 180 / pi;
-					std::cout << degree << std::endl;
+					//std::cout << degree << std::endl;
 					trans::rotate_transformer<boost::geometry::degree, double, 2, 2> rotate(degree);
-					auto dp = p1;
-					subtract_point(dp, pPiece1);
-					trans::translate_transformer<double, 2, 2> translate(dp.x(), dp.y());
+					auto despoint = p1;
+					subtract_point(despoint, pPiece1);
+					trans::translate_transformer<double, 2, 2> translate(despoint.x(), despoint.y());
 					auto matrix = rotate.matrix()*translate.matrix();
 					trans::matrix_transformer<double, 2, 2> rotateTranslate(matrix);
 					polygon b;
-					transform(a, b, rotateTranslate);
+					transform(tagramP, b, rotateTranslate);
 					for_each_point(b, [](point& p) {
 						round<5>(p);
+						//std::cout << p.x() << " " << p.y() << std::endl;
 					});
-					std::cout << dsv(b) << std::endl;
-					std::cout << within(b,outerRing) << std::endl;
-				}				
+
+					mpolygon differs;
+					polygon* silhouttet;
+					bool isWithin = within(b, silhouettePolygon);
+					//std::cout << isWithin << std::endl;
+					if (isWithin == false) {
+						difference(b, silhouettePolygon, differs);
+						if (differs.size() == 0)
+						{
+							isWithin = true;
+						}
+						else
+						{
+							double area = 0;
+							BOOST_FOREACH(const auto& p, differs)
+							{
+								area += boost::geometry::area(p);
+							}
+							if (area < MIN_AREA_THRESHOLD)
+							{
+								isWithin == true;
+							}
+						}
+					}
+
+					if (isWithin == true)
+					{
+						difference(silhouettePolygon, b, differs);
+						if (differs.size() > 0)
+						{
+							//std::cout << "xin chao " << differs.size() << std::endl;
+							for (auto it = differs.begin(); it != differs.end();)
+							{
+								if (area(*it) < MIN_AREA_THRESHOLD) {
+									it = differs.erase(it);
+								}
+								else {
+									++it;
+								}
+							}
+						}
+						std::cout << "xin chao, area differ is " << area(differs) << std::endl;
+						Mat m = Mat::zeros(MAXSIZE, MAXSIZE, CV_8UC3);
+						
+						polywrite(m, differs);
+						imwrite("./image/abc.png", m);						
+					}
+				}
 				break;
 			}
 			break;
 		}
 	}
-
-	Mat image = Mat::zeros(400, 400, CV_8UC3);
-
-	int w = 400;
-	// Draw a circle 
-	/** Create some points */
-	cv::Point rook_points[1][20];
-	rook_points[0][0] = cv::Point(w / 4.0, 7 * w / 8.0);
-	rook_points[0][1] = cv::Point(3 * w / 4.0, 7 * w / 8.0);
-	rook_points[0][2] = cv::Point(3 * w / 4.0, 13 * w / 16.0);
-	rook_points[0][3] = cv::Point(11 * w / 16.0, 13 * w / 16.0);
-	rook_points[0][4] = cv::Point(19 * w / 32.0, 3 * w / 8.0);
-	rook_points[0][5] = cv::Point(3 * w / 4.0, 3 * w / 8.0);
-	rook_points[0][6] = cv::Point(3 * w / 4.0, w / 8.0);
-	rook_points[0][7] = cv::Point(26 * w / 40.0, w / 8.0);
-	rook_points[0][8] = cv::Point(26 * w / 40.0, w / 4.0);
-	rook_points[0][9] = cv::Point(22 * w / 40.0, w / 4.0);
-	rook_points[0][10] = cv::Point(22 * w / 40.0, w / 8.0);
-	rook_points[0][11] = cv::Point(18 * w / 40.0, w / 8.0);
-	rook_points[0][12] = cv::Point(18 * w / 40.0, w / 4.0);
-	rook_points[0][13] = cv::Point(14 * w / 40.0, w / 4.0);
-	rook_points[0][14] = cv::Point(14 * w / 40.0, w / 8.0);
-	rook_points[0][15] = cv::Point(w / 4.0, w / 8.0);
-	rook_points[0][16] = cv::Point(w / 4.0, 3 * w / 8.0);
-	rook_points[0][17] = cv::Point(13 * w / 32.0, 3 * w / 8.0);
-	rook_points[0][18] = cv::Point(5 * w / 16.0, 13 * w / 16.0);
-	rook_points[0][19] = cv::Point(w / 4.0, 13 * w / 16.0);
-
-	const cv::Point* ppt[1] = { rook_points[0] };
-	int npt[] = { 20 };
-
-	cv::fillPoly(image, ppt, npt, 1, Scalar(255, 255, 255), 8);
-	auto b = imwrite("/data/local/tmp/output.png", image);
-	std::cout << "Xin chao tat ca the gioi nay :" << b << std::endl;
-
-	//	auto  outerRing = largeTriangular1.outer();
-	//	auto blackSize = outerRing.size();
-	//	std::cout << blackSize << std::endl;
-	//	auto innerRings = largeTriangular1.inners();
-	//	auto numInnerRings = innerRings.size();
-	//
-	//	auto iswithin = within(largeTriangular1, largeTriangular2);
-	//std:cout << " is within : ? " << iswithin << std::endl;
-	//
-	//	const auto& remain = silhouettePolygon.outer();
-	//	
-	//std::list<polygon> output;
-	//boost::geometry::difference(green, green, output);
-	//
-	//std::cout << "number of remain polygon" << output.size() << std::endl;
-
-	//std::cout << "green - blue:" << std::endl;
-	//BOOST_FOREACH(polygon const& p, output)
-	//{
-	//	//auto squarePoint = green.outer().size();
-	//	//std::wcout << "Number points of square is : " << squarePoint << std::endl;
-	//	auto r = p.outer();
-	//	auto numberPoint = r.size();
-	//	std::cout << "Number point of polygon is : " << numberPoint << std::endl;
-	//	for (int i=0;i<numberPoint;i++)
-	//	{
-	//		auto point = r.at(i);
-	//		double x = point.x();
-	//		std::cout << x <<" "<<point.y()<< std::endl;
-	//	}
-	//}
-
-	//return 0;
 }
+
--- GEOS/Source2.cpp
+++ GEOS/Source2.cpp
@@ -1,77 +1,44 @@
-//#include <iostream>
-//#include <geos\geom\MultiPolygon.h>
-//#include <geos\geom\GeometryFactory.h>
-//#include <geos.h>
-//#include <math.h>
-//#include <geos\algorithm\CGAlgorithms.h>
-//
-//#include <list>
-//#include <boost/geometry.hpp>
-//#include <boost/geometry/geometries/point_xy.hpp>
-//#include <boost/geometry/geometries/polygon.hpp>
-//#include <boost/foreach.hpp>
-//
 //#include <opencv2/core/core.hpp>
 //#include <opencv2/highgui/highgui.hpp>
 //#include <opencv2/imgproc/imgproc.hpp>
-//#include <exception>
 //using namespace cv;
-//using namespace boost::geometry;
-//using namespace geos::geom;
-//using namespace std;
-//using namespace geos::algorithm;
-//namespace trans = boost::geometry::strategy::transform;
 //
-//typedef boost::geometry::model::d2::point_xy<double> point, vector2d;
-//typedef boost::geometry::model::polygon<boost::geometry::model::d2::point_xy<double>> polygon;
-//typedef boost::geometry::model::multi_polygon<polygon> mpolygon;
-//const long double pi = boost::math::constants::pi<long double>();
-//
-//double division(int a, int b) {
-//	if (b == 0) {
-//		throw " sd ";
-//	}
-//	return (a / b);
-//}
-//
-//void imgwrite(cv::Mat mat,const polygon& poly)
+//int main()
 //{
-//	//auto outer = poly.outer();
-//	//auto inners = poly.inners();
-//	//cv::Point pt_outer[1][30];
-//	//const cv::Point *pts[1] = { pt_outer[0] };
-//	//
-//	//auto num = num_points(outer);
-//	if (3 > 2) {
-//		throw "cannot print the poly, because numpoints exceed limit ";
-//		return;
-//	}
-//	/*int npts[1] = { num };
+//	// Create black empty images
+//	Mat image = Mat::zeros(400, 400, CV_8UC3);
 //
-//	for (int i =0;i<num-1;i++)
-//	{
-//	auto p = outer.at(i);
-//	pt_outer[0][i] = cv::Point(p.x()*10, p.y()*10);
-//	}
+//	int w = 400;
+//	// Draw a circle 
+//	/** Create some points */
+//	Point rook_points[1][5];
+//	rook_points[0][0] = Point(0, 20);
+//	rook_points[0][1] = Point(0, 28.2843);
+//	rook_points[0][2] = Point(28.2843, 28.2843);
+//	rook_points[0][3] = Point(28.2843, 0);
+//	rook_points[0][4] = Point(20, 0);
+//	rook_points[0][5] = Point(3 * w / 4.0, 3 * w / 8.0);
+//	rook_points[0][6] = Point(3 * w / 4.0, w / 8.0);
+//	rook_points[0][7] = Point(26 * w / 40.0, w / 8.0);
+//	rook_points[0][8] = Point(26 * w / 40.0, w / 4.0);
+//	rook_points[0][9] = Point(22 * w / 40.0, w / 4.0);
+//	rook_points[0][10] = Point(22 * w / 40.0, w / 8.0);
+//	rook_points[0][11] = Point(18 * w / 40.0, w / 8.0);
+//	rook_points[0][12] = Point(18 * w / 40.0, w / 4.0);
+//	rook_points[0][13] = Point(14 * w / 40.0, w / 4.0);
+//	rook_points[0][14] = Point(14 * w / 40.0, w / 8.0);
+//	rook_points[0][15] = Point(w / 4.0, w / 8.0);
+//	rook_points[0][16] = Point(w / 4.0, 3 * w / 8.0);
+//	rook_points[0][17] = Point(13 * w / 32.0, 3 * w / 8.0);
+//	rook_points[0][18] = Point(5 * w / 16.0, 13 * w / 16.0);
+//	rook_points[0][19] = Point(w / 4.0, 13 * w / 16.0);
 //
-//	cv::fillPoly(mat, pts, npts, 1, Scalar(255, 255, 255), 1);*/
-//	//auto b = imwrite("./a.png", mat);
-//	//std::cout << "is writed ? " << b << std::endl;
-//}
+//	const Point* ppt[1] = { rook_points[0] };
+//	int npt[] = { 5 };
 //
-//int main() {
-//	int x = 50;
-//	int y = 0;
-//	double z = 0;
-//	polygon poly;
-//	try {
-//		imgwrite(Mat::zeros(1,1,CV_8UC3), poly);
-//		z = division(x, y);
-//		cout << z << endl;
-//	}
-//	catch (const char* msg) {
-//		cerr << msg << endl;
-//	}
+//	cv::fillPoly(image, ppt, npt, 1, Scalar(255, 255, 255), 8);
+//	imshow("Image", image);
 //
-//	return 0;
+//	waitKey(0);
+//	return(0);
 //}
